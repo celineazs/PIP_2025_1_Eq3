262190d84c8ed2f7f33d35d8ce102699fa97df1d
celineazs <a2223339025@alumnos.uat.edu.mx>
2025-02-02 17:27:58 -0600

    Tarea Unidad 2

--- Unidad 2/Tarea Unidad 2/T03_Mililitros_aLitros.py
+++ Unidad 2/Tarea Unidad 2/T03_Mililitros_aLitros.py
@@ -1,24 +1,31 @@
 import sys
 from PyQt5 import uic, QtWidgets, QtGui
-qtCreatorFile = "P08_DescripcionDeImagenes.ui"
+qtCreatorFile = "T03_Mililitros_alitros.ui"
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
-        self.selectorImagen.setMinimum(1)
-        self.selectorImagen.setMaximum(1)
-        self.selectorImagen.setSingleStep(1)
-        self.selectorImagen.setValue(1) #valor inicial
-        self.selectorImagen.valueChanged.connect(self.cambiaValor)
-        #self.txt_valor.setText("1")
+        self.verticalSlider.setMinimum(0)  # Mínimo en mililitros
+        self.verticalSlider.setMaximum(5000)  # Máximo en mililitros (5 litros)
+        self.verticalSlider.setSingleStep(100)  # Paso de 100 ml
+        self.verticalSlider.setValue(0)  # Valor inicial
 
+        self.verticalSlider.valueChanged.connect(self.cambiaValor)
+        self.txt_valor.setText("0 ml")
+        self.txt_litros.setText("0.00 L")
+
+        self.btn_convertir.clicked.connect(self.convertir)
 
     def cambiaValor(self):
-        pass
-        #value=self.selectorImagen.value()
-        #self.txt_valor.setText(str(value))
+        value_ml = self.verticalSlider.value()
+        self.txt_valor.setText(f"{value_ml} ml")
+
+    def convierte(self):
+        value_ml = self.verticalSlider.value()
+        value_l = value_ml / 1000
+        self.txt_litros.setText(f"{value_l:.2f} L")
 
 
 if __name__ == "__main__":
